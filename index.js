07043c41b7fa22d3d7fb33e786ca4ce6fabafe9d
Kapil <kapilguptakg1519@gmail.com>
2025-11-29 22:05:45 +0530

    updates

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import Contact from './contactModel.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
